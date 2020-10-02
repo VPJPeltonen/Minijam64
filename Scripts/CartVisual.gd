117d79c4f07ea3cc5cbc15231e2516a509f25b1d
VPJP <vpjpeltonen@gmail.com>
2020-10-02 22:34:08 +0300

    basic sprites for player

--- Scripts/CartVisual.gd
+++ Scripts/CartVisual.gd
@@ -6,12 +6,19 @@ var drifting = false
 func _process(delta):
 	if !GAME.race_on:
 		return
-	if Input.get_joy_axis(0,0) >= 0.2 or Input.get_joy_axis(0,0) <= -0.2:
+	$AnimatedSprite3D.play("forward")
+	if Input.get_joy_axis(0,0) >= 0.2 :
 		rotate_y(delta*turn_speed*-Input.get_joy_axis(0,0))
+		$AnimatedSprite3D.play("right")
+	if Input.get_joy_axis(0,0) <= -0.2:
+		rotate_y(delta*turn_speed*-Input.get_joy_axis(0,0))
+		$AnimatedSprite3D.play("left")
 	else:
 		if Input.is_action_pressed("left"):
 			rotate_y(delta*turn_speed)
+			$AnimatedSprite3D.play("left")
 		if Input.is_action_pressed("right"):
 			rotate_y(-delta*turn_speed)
+			$AnimatedSprite3D.play("right")
 	transform.origin = get_parent().get_node("Cart").transform.origin
 
