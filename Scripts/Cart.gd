4963ba04383070423c200d40e71a50c4632a652d
VPJP <vpjpeltonen@gmail.com>
2020-10-03 12:30:53 +0300

    some sort of drifting

--- Scripts/Cart.gd
+++ Scripts/Cart.gd
@@ -5,6 +5,8 @@ var break_speed = 1.0
 
 onready var target = get_parent().get_node("Visual/Target")
 onready var main = get_parent().get_parent()
+onready var drift_effect1 = get_parent().get_node("Visual/DriftEffect")
+onready var drift_effect2 = get_parent().get_node("Visual/DriftEffect2")
 
 var on_road = true
 
@@ -13,10 +15,7 @@ func _process(delta):
 	if !GAME.race_on:
 		return
 	var dir = global_transform.origin - target.global_transform.origin
-	if Input.is_action_pressed("drift"):
-		linear_damp = 0.1
-	else:
-		linear_damp = 2.0
+	check_drift()
 	if Input.is_action_pressed("accelerate"):
 		if on_road and !Input.is_action_pressed("drift"):
 			add_central_force(dir * (-delta*move_speed))
@@ -25,6 +24,17 @@ func _process(delta):
 	if Input.is_action_pressed("break") and on_road:
 		add_central_force(dir * (delta*move_speed*0.25))
 
+
+func check_drift():
+	if Input.is_action_pressed("drift"):
+		#linear_damp = 0.1
+		drift_effect1.emitting = true
+		drift_effect2.emitting = true
+	else:
+		#linear_damp = 2.0
+		drift_effect1.emitting = false
+		drift_effect2.emitting = false
+		
 func Boost(delta):
 	var dir = global_transform.origin - target.global_transform.origin
 	add_central_force(dir * (-delta*move_speed) * 2)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,8 +10,22 @@ export var break_speed = 1.0
 var current_checkpoint = 0
 var current_lap = 1
 
+onready var drift_effect1 = $Cart/Visual/DriftEffect
+onready var drift_effect2 = $Cart/Visual/DriftEffect2
+
 func _ready():
 	set_stats()
+	
+func _process(delta):
+	check_drift()
+
+func check_drift():
+	if Input.is_action_pressed("drift"):
+		$Cart/Visual.turn_speed = turn_speed/2
+		$Cart/Cart.linear_damp = 0.1
+	else:
+		$Cart/Visual.turn_speed = turn_speed
+		$Cart/Cart.linear_damp = 2.0
 
 func set_stats():
 	$Cart/Cart.move_speed = move_speed
