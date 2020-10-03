dc9a55f514661271bfc0224d4e18e3190c0c9905
VPJP <vpjpeltonen@gmail.com>
2020-10-03 21:59:38 +0300

    jump balloon mechanic

---
+++ Scripts/Balloon.gd
@@ -0,0 +1,20 @@
+extends Area
+
+var boostable = []
+var boost_amount = 4000
+
+func _process(delta):
+	for cart in boostable:
+		cart.add_central_force(Vector3.UP*delta*boost_amount)
+		print("doing")
+
+func _on_Balloon_body_entered(body):
+	if body.is_in_group("Cart"):
+		if !boostable.has(body):
+			boostable.append(body)
+
+func _on_Balloon_body_exited(body):
+	if body.is_in_group("Cart"):
+		if boostable.has(body):
+			boostable.erase(body)
+
