599a3bc72ee9d5a118ace2e5c93e0951c4f2e9b7
Fekinox <williamowenfowlk@gmail.com>
2020-10-04 14:35:24 -0400

    added music/sound player nodes

---
+++ Scenes/MusicPlayer.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var song_map : Dictionary = {}
+var currentSong : AudioStreamPlayer = null
+
+func _ready():
+	# Add AudioStreamPlayers as children to this node
+	for songPlayer in get_children():
+		song_map[songPlayer.name] = songPlayer
+		
+func play(song : String):
+	# Pause current song
+	if currentSong != null:
+		currentSong.stop()
+		
+	# Load new song
+	if (song_map.has(song)):
+		song_map[song].play(0)
+		
+func pause(): if currentSong != null: currentSong.pause()
+
+func unpause(): if currentSong != null: currentSong.play()
---
+++ Scenes/SoundPlayer.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var sound_map : Dictionary = {}
+
+func _ready():
+	# Add AudioStreamPlayers as children to this node
+	for soundPlayer in get_children():
+		sound_map[soundPlayer.name] = soundPlayer
+		
+func play(sound : String):
+	# Load new sound
+	if (sound_map.has(sound)):
+		sound_map[sound].play(0)
+	
+func stopAll():
+	for sound in sound_map.values(): sound.stop()
