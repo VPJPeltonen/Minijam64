a47ed21f914e6c98b2421b51e0c1ae5cac5de6d6
VPJP <vpjpeltonen@gmail.com>
2020-10-04 21:21:58 +0300

    nuke animation

--- Scripts/Explosion.gd
+++ Scripts/Explosion.gd
@@ -3,9 +3,13 @@ extends Area
 var size = Vector3(1.0,1.0,1.0)
 var grow_speed = Vector3(1.0,1.0,1.0)
 
+func _ready():
+	$AnimatedSprite3D.play("default")
+
 func _process(delta):
 	size += grow_speed*delta*4
-	scale = size
+	$MeshInstance.scale = size
+	$CollisionShape.scale = size
 
 func _on_Timer_timeout():
 	queue_free()
