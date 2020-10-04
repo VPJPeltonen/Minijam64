f441ed1f128342c2c3f770e1a5fdb2659ff57e66
VPJP <vpjpeltonen@gmail.com>
2020-10-04 18:00:37 +0300

    powerups to map

--- Scripts/PowerUp.gd
+++ Scripts/PowerUp.gd
@@ -3,13 +3,16 @@ extends Area
 var active = true
 var powerup = "boost"
 
+func _ready():
+	$Itembox/AnimationPlayer.play("Cube021Action")
+
 func _on_Timer_timeout():
 	active = true
-	$MeshInstance.show()
+	$Itembox.show()
 
 func _on_PowerUp_body_entered(body):
 	if body.is_in_group("Cart") and active and body.power_up == null:
 		body.GainPowerUp(powerup)
-		$MeshInstance.hide()
+		$Itembox.hide()
 		active = false
 		$Timer.start()
